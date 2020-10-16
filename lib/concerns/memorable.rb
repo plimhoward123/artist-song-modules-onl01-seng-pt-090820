0f563336857c9161e8503ec3e6affb09636729de
Plimon Howard <plimon.howard@gmail.com>
2020-10-16 21:32:33 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,16 @@
 module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-  def count
-    self.all.count
+    def count
+      self.all.count
+    end
   end
-
-  def reset_all
-    self.all.clear
+  module InstanceMethods
+    def initialize
+      # some more code coming soon!
+    end
   end
 end
