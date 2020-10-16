83e10a5ac8ee15cf24a5d9e94a26a02507ec6911
Plimon Howard <plimon.howard@gmail.com>
2020-10-16 21:19:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,15 +4,12 @@ class Song
   attr_accessor :name
   attr_reader :artist
   extend Memorable
+  extend Findable
 
   @@songs = []
 
   def initialize
     @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
   end
 
   def self.all
