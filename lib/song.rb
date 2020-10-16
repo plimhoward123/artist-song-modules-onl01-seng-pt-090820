15ec05d2899b66826462f3cf84991a6d47cbdc88
Plimon Howard <plimon.howard@gmail.com>
2020-10-16 21:08:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,6 +18,14 @@ class Song
   def self.all
     @@songs
   end
+  
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
 
   def artist=(artist)
     @artist = artist
